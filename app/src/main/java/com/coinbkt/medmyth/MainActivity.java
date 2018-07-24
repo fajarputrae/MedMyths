1acdd168a13cb5d58763106cef941789fdd3ea66
rezyfr <frotylatz@gmail.com>
2018-07-24 11:39:49 +0700

    added Facts and Main Menu Pages

--- app/src/main/java/com/coinbkt/medmyth/MainActivity.java
+++ app/src/main/java/com/coinbkt/medmyth/MainActivity.java
@@ -1,13 +1,26 @@
 package com.coinbkt.medmyth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnFacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFacts = findViewById(R.id.factsBtn);
+        btnFacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FactsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
