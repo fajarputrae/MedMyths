497e904b47c19176ef63d630fe4d8513831c004b
rezyfr <frotylatz@gmail.com>
2018-07-29 09:58:10 +0700

    added Splashscreen, facts&mainmenu rev

--- app/src/main/java/com/coinbkt/medmyth/FactsActivity.java
+++ app/src/main/java/com/coinbkt/medmyth/FactsActivity.java
@@ -1,7 +1,10 @@
 package com.coinbkt.medmyth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class FactsActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class FactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facts);
+
+        ImageButton backButton = findViewById(R.id.backBtn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FactsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
